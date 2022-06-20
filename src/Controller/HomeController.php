052646a3d1974bc12016d7c6b29d6c5afeb7d30a
Pierre DRILLIN <sio.drillin@gmail.com>
2022-06-20 17:17:36 +0200

    WIP submitted form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        dump($request);
         return $this->render('index.html.twig', [
             'controller_name' => 'HomeController',
             'id_modal_map_new' => 'newMapForm',
--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 
 use App\Form\MapType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MapController extends AbstractController
 {
     #[Route(path: '/map/new', name: 'new_map')]
-    public function new(): Response
+    public function new(Request $request): Response
     {
+        dump($request);
         // get the login error if there is one
         $form = $this->createForm(MapType::class);
         
