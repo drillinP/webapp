1c591ff5d3f1d1e6abb884c429dcc0a619eb3e30
Pierre DRILLIN <sio.drillin@gmail.com>
2022-06-23 18:46:37 +0200

    Testing use doctrine drivermanager to exec query

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\DriverManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +14,24 @@ class HomeController extends AbstractController
     public function index(Request $request): Response
     {
         dump($request);
+        $conn = DriverManager::getConnection(['url' => $this->getParameter('conn_params'), 'driver' => 'pdo_pgsql']);
+//        to run create query e.g. create schema
+        /*try {
+            $statement = $conn->prepare('Create schema data;');
+            $resultSet = $statement->executeQuery();
+        }catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }*/
+
+//        Select all table in schema data
+        try {
+            $statement = $conn->prepare("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'data'");
+            $resultSet = $statement->executeQuery();
+            $tables = $resultSet->fetchAllAssociative();
+        }catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
+
         return $this->render('index.html.twig', [
             'controller_name' => 'HomeController',
             'id_modal_map_new' => 'newMapForm',
