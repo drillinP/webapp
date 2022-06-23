e0b9cd1f2a49c91ab069fff147df541214b4c741
Pierre DRILLIN <sio.drillin@gmail.com>
2022-06-23 18:52:56 +0200

    Update index: * Testing toast * Differentiate if the user is logged or not block body for connected user or block body_unconnected for login page * adding JQuery and Use ajax to send form * Update MapController to return form submitted response

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\MapOptionsType;
 use App\Form\MapType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,12 +18,30 @@ class MapController extends AbstractController
     {
         dump($request);
         // get the login error if there is one
-        $form = $this->createForm(MapType::class);
-        
-        var_dump('test');
-        
+        $formMap = $this->createForm(MapType::class);
+        $formMap->handleRequest($request);
+        if ($formMap->isSubmitted() && $formMap->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $map = $formMap->getData();
+            // ... perform some action, such as saving the task to the database
+
+            return new JsonResponse($map);
+        }
         return $this->render('map/create.html.twig', [
-            'formMap' => $form->createView(),
+            'formMap' => $formMap->createView(),
+        ]);
+    }
+
+    #[Route(path: '/map/{id<\d+>}', name: 'project_map')]
+    public function show(Request $request): Response
+    {
+        dump($request);
+        // get the login error if there is one
+        $formMap = $this->createForm(MapType::class);
+
+        return $this->render('map/create.html.twig', [
+            'formMap' => $formMap->createView(),
         ]);
     }
 }
